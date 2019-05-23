643046a3340355d44db8683f68682a6c7873593e
m4rc1o <marcioinaciosantana@gmail.com>
2019-05-23 19:57:21 -0300

    Adding the disciplines component, #48

---
+++ Frontend/src/views/Disciplines.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="projects">
+        <h1 class="font-weight-regular blue-grey--text text--darken-4 text-md-center">Disciplinas</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
