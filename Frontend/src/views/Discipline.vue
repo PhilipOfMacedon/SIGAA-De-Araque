581890faa7ea6ee71bd88174726c978230f22fb4
m4rc1o <marcioinaciosantana@gmail.com>
2019-05-24 16:47:50 -0300

    Adding disciplines name, #60

--- Frontend/src/views/Discipline.vue
+++ Frontend/src/views/Discipline.vue
@@ -1,10 +1,19 @@
 <template>
-    
+    <div class="disciplines">
+    <v-container fluid class="lighten-3 secondary">
+        <h1 class="font-weight-regular blue-grey--text text--darken-4 ml-3">Nome da disciplina</h1>
+    </v-container>
+</div>
+
 </template>
 
 <script>
 export default {
+    data(){
+        return{
 
+        }
+    }
 }
 </script>
 
