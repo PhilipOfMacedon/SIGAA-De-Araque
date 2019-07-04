5a7d9053c877cbcbaddccc81af6c724100a947ce
Filipe Rodrigues <filiperodrigues97@gmail.com>
2019-07-04 05:29:35 -0300

    Adjusting server to allow CORS, #96

--- Backend/sgp/src/main/java/br/com/sgp/SgpApplication.java
+++ Backend/sgp/src/main/java/br/com/sgp/SgpApplication.java
@@ -1,7 +1,15 @@
 package br.com.sgp;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SgpApplication {
@@ -10,4 +18,19 @@ public class SgpApplication {
 		SpringApplication.run(SgpApplication.class, args);
 	}
 
+	@Bean
+    public FilterRegistrationBean simpleCorsFilter() {  
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
+        CorsConfiguration config = new CorsConfiguration();  
+        config.setAllowCredentials(true); 
+        // *** URL below needs to match the Vue client URL and port ***
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080")); 
+        config.setAllowedMethods(Collections.singletonList("*"));  
+        config.setAllowedHeaders(Collections.singletonList("*"));  
+        source.registerCorsConfiguration("/**", config);  
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);  
+        return bean;  
+    }   
+	
 }
